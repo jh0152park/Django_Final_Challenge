9fd56ab654df67fb853fe528e81e7fe0ccdf6577
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-06 18:38:34 +0900

    added basic structure

--- marvel/src/index.tsx
+++ marvel/src/index.tsx
@@ -1,7 +1,39 @@
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
 import ReactDOM from "react-dom/client";
+import { reset } from "styled-reset";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  body {
+    background-color: white;
+    color: black;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+`;
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+
+root.render(
+    <>
+        <GlobalStyles />
+        <ChakraProvider>
+            <RecoilRoot>
+                <QueryClientProvider client={queryClient}>
+                    <App />
+                </QueryClientProvider>
+            </RecoilRoot>
+        </ChakraProvider>
+    </>
+);
