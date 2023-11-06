6da00d6b7e22b76679dfc16ff87e57dcd9c09818
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-06 22:20:21 +0900

    added favicon

--- marvel/src/index.tsx
+++ marvel/src/index.tsx
@@ -7,6 +7,8 @@ import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./global.css";
 
+// marvel logo: https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/220px-Marvel_Logo.svg.png
+
 const GlobalStyles = createGlobalStyle`
   ${reset}
   body {
--- marvel/src/screen/Intro.tsx
+++ marvel/src/screen/Intro.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, HStack, Heading, Image, VStack } from "@chakra-ui/react";
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 
 const SCREEN_HEIGHT = window.innerHeight;
 
@@ -11,66 +12,73 @@ export default function Intro() {
     }
 
     return (
-        <Box w="100%" h="100vh" position="relative">
-            <Image
-                w="100%"
-                h="100%"
-                objectFit="cover"
-                src={require("../resource/images/welcome.jpg")}
-            />
+        <>
+            <Helmet>
+                <title>Marvel Universe</title>
+            </Helmet>
+            <Box w="100%" h="100vh" position="relative">
+                <Image
+                    w="100%"
+                    h="100%"
+                    objectFit="cover"
+                    src={require("../resource/images/welcome.jpg")}
+                />
 
-            <Center w="100%" h="100%" position="absolute" top="0" left="0">
-                <VStack spacing="30px" zIndex="99">
-                    <Heading fontFamily="Bungee Spice" fontSize="70px">
-                        LET'S EXPLORE
-                    </Heading>
-                    <Heading fontFamily="Bungee Spice" fontSize="70px">
-                        MARVEL COMICS UNIVERSE
-                    </Heading>
-                    <Heading fontFamily="Bungee Spice" fontSize="70px">
-                        IF YOU READY,
-                    </Heading>
-                    <Heading fontFamily="Bungee Spice" fontSize="70px">
-                        SAY
-                    </Heading>
-                    <Heading
-                        fontFamily="Bungee Spice"
-                        fontSize="70px"
-                        _hover={{
-                            cursor: "pointer",
-                        }}
-                        onMouseOver={toggleHover}
-                        onMouseLeave={toggleHover}
+                <Center w="100%" h="100%" position="absolute" top="0" left="0">
+                    <VStack spacing="30px" zIndex="99">
+                        <Heading fontFamily="Bungee Spice" fontSize="70px">
+                            LET'S EXPLORE
+                        </Heading>
+                        <Heading fontFamily="Bungee Spice" fontSize="70px">
+                            MARVEL COMICS UNIVERSE
+                        </Heading>
+                        <Heading fontFamily="Bungee Spice" fontSize="70px">
+                            IF YOU READY,
+                        </Heading>
+                        <Heading fontFamily="Bungee Spice" fontSize="70px">
+                            SAY
+                        </Heading>
+                        <Heading
+                            fontFamily="Bungee Spice"
+                            fontSize="70px"
+                            _hover={{
+                                cursor: "pointer",
+                            }}
+                            onMouseOver={toggleHover}
+                            onMouseLeave={toggleHover}
+                        >
+                            HEIMDALL OPEN THE BIFROST !!
+                        </Heading>
+                    </VStack>
+
+                    <HStack
+                        position="absolute"
+                        bottom={SCREEN_HEIGHT}
+                        spacing={0}
+                        opacity={isHover ? 1 : 0}
+                        transition={"all 0.5s linear"}
+                        transform={
+                            isHover ? `translateY(${SCREEN_HEIGHT}px)` : ""
+                        }
                     >
-                        HEIMDALL OPEN THE BIFROST !!
-                    </Heading>
-                </VStack>
-
-                <HStack
-                    position="absolute"
-                    bottom={SCREEN_HEIGHT}
-                    spacing={0}
-                    opacity={isHover ? 1 : 0}
-                    transition={"all 0.5s linear"}
-                    transform={isHover ? `translateY(${SCREEN_HEIGHT}px)` : ""}
-                >
-                    <Box
-                        w="50px"
-                        h="100vh"
-                        background="linear-gradient(to right, white, red, orange, yellow, green, blue, indigo, purple, white)"
-                    ></Box>
-                    <Box
-                        w="50px"
-                        h="100vh"
-                        background="linear-gradient(to right, white, red, orange, yellow, green, blue, indigo, purple, white)"
-                    ></Box>
-                    <Box
-                        w="50px"
-                        h="100vh"
-                        background="linear-gradient(to right, white, red, orange, yellow, green, blue, indigo, purple, white)"
-                    ></Box>
-                </HStack>
-            </Center>
-        </Box>
+                        <Box
+                            w="50px"
+                            h="100vh"
+                            background="linear-gradient(to right, white, red, orange, yellow, green, blue, indigo, purple, white)"
+                        ></Box>
+                        <Box
+                            w="50px"
+                            h="100vh"
+                            background="linear-gradient(to right, white, red, orange, yellow, green, blue, indigo, purple, white)"
+                        ></Box>
+                        <Box
+                            w="50px"
+                            h="100vh"
+                            background="linear-gradient(to right, white, red, orange, yellow, green, blue, indigo, purple, white)"
+                        ></Box>
+                    </HStack>
+                </Center>
+            </Box>
+        </>
     );
 }
