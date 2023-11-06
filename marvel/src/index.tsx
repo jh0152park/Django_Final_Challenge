74fec4e3bfd2bf01b975ab6ff60be187f5cf1281
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-06 22:04:37 +0900

    created intro page

--- marvel/src/index.tsx
+++ marvel/src/index.tsx
@@ -1,10 +1,11 @@
-import { createGlobalStyle } from "styled-components";
 import App from "./App";
 import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./global.css";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
