098c102fe024366c4868c7e228e8e6200c50b9be
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-07 20:43:36 +0900

    added comics list

--- marvel/src/screen/Comics.tsx
+++ marvel/src/screen/Comics.tsx
@@ -5,13 +5,33 @@ import { listComics } from "../api";
 import ComicsBanner from "../components/ComicsBanner";
 import Title from "./Comics/Title";
 import Loading from "./Comics/Loading";
+import { ComicsResponse } from "../types";
+import Comic from "./Comics/Comic";
 
 export default function Comics() {
     const loadingArray = Array.from({ length: 20 }, () => 0);
-    const { isLoading, data } = useQuery(["comics"], listComics);
+    const { isLoading, data } = useQuery<ComicsResponse>(
+        ["comics"],
+        listComics
+    );
     const Comics = data?.data.results;
 
-    console.log(Comics);
+    // if (!isLoading && Comics) {
+    //     const id = Comics[0].id;
+    //     const title = Comics[0].title;
+    //     const image = Comics[0].thumbnail.path;
+    //     const type = Comics[0].thumbnail.extension;
+    //     const thumbnail = `${image}.${type}`;
+    //     const creator_number = Comics[0].creators.available;
+    //     const creators = Comics[0].creators.items;
+    //     const genre = Comics[0].format;
+
+    //     console.log(id);
+    //     console.log(title);
+    //     console.log(thumbnail);
+    //     console.log(genre);
+    //     console.log(creators);
+    // }
 
     return (
         <>
@@ -23,19 +43,18 @@ export default function Comics() {
             <Box w="100%" px="350px" pt="100px">
                 <Title />
                 <Grid templateColumns="repeat(5, 1fr)" gap="20px">
-                    {/* <Box
-                        w="100%"
-                        h="350px"
-                        bg="blue"
-                        mb="20px"
-                        boxShadow="0 2px 2px rgba(0,0,0,0.15), 
-                        0 8px 8px rgba(0,0,0,0.15), 
-                        0 15px 15px rgba(0,0,0,0.15), 
-                        0 20px 20px rgba(0,0,0,0.15)"
-                    /> */}
-                    {loadingArray.map((dummy) => (
-                        <Loading key={dummy} />
-                    ))}
+                    {isLoading
+                        ? loadingArray.map((dummy, index) => (
+                              <Loading key={index} />
+                          ))
+                        : Comics?.map((comic) => (
+                              <Comic
+                                  key={comic.id}
+                                  title={comic.title}
+                                  creator={comic.creators.items}
+                                  thumbnail={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+                              />
+                          ))}
                 </Grid>
             </Box>
         </>
