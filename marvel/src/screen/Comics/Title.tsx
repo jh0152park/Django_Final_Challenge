14a76dccdd543fa441d875e0ed9a47e10b2c142a
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-07 16:00:03 +0900

    added comics title

--- marvel/src/screen/Comics/Title.tsx
+++ marvel/src/screen/Comics/Title.tsx
@@ -1,5 +1,19 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 export default function Title() {
-    return <Heading>12</Heading>;
+    return (
+        <Box position="relative" mb="50px">
+            <Box w="80px" h="3px" bg="gold" transform="rotateZ(-35deg)" />
+            <Heading
+                fontFamily="Roboto Condensed"
+                fontSize="30px"
+                zIndex="99"
+                position="absolute"
+                top="-15px"
+                left="10px"
+            >
+                BEST SELLING DIGITAL COMICS
+            </Heading>
+        </Box>
+    );
 }
