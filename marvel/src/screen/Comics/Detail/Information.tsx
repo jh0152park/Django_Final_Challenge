bf0939b3f5b2234a507a4ae326a8c1faaa477f8c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-08 14:38:25 +0900

    added go back and go comics button at comic charaters detail

--- marvel/src/screen/Comics/Detail/Information.tsx
+++ marvel/src/screen/Comics/Detail/Information.tsx
@@ -22,7 +22,7 @@ interface IProps {
     artist: string;
 }
 
-const MyButton = styled.div`
+export const MyButton = styled.div`
     width: 150px;
     height: 50px;
     display: flex;
@@ -31,16 +31,16 @@ const MyButton = styled.div`
     color: rgba(255, 255, 255, 0.5);
     font-weight: bold;
     font-family: "Roboto Condense";
-    background: linear-gradient(-45deg, transparent 15px, #f56565 0) right,
-        linear-gradient(135deg, transparent 15px, #f56565 0) left;
+    background: linear-gradient(-45deg, transparent 15px, #e53e3e 0) right,
+        linear-gradient(135deg, transparent 15px, #e53e3e 0) left;
     background-size: 50% 100%;
     background-repeat: no-repeat;
     transition: all 0.2s linear;
     &:hover {
         color: white;
         cursor: pointer;
-        background: linear-gradient(-45deg, transparent 15px, #e53e3e 0) right,
-            linear-gradient(135deg, transparent 15px, #e53e3e 0) left;
+        background: linear-gradient(-45deg, transparent 15px, #c53030 0) right,
+            linear-gradient(135deg, transparent 15px, #c53030 0) left;
         background-size: 50% 100%;
         background-repeat: no-repeat;
     }
