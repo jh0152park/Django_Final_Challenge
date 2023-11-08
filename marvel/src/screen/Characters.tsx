bf10b168ee469a51f2f45fb26cae4657d65aa804
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-08 16:44:01 +0900

    created characters skelton

--- marvel/src/screen/Characters.tsx
+++ marvel/src/screen/Characters.tsx
@@ -1,8 +1,11 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Grid, Heading } from "@chakra-ui/react";
 import { Helmet } from "react-helmet";
 import CharactersBanner from "../components/CharactersBanner";
+import Loading from "./Characters/Loading";
 
 export default function Characters() {
+    const loadingArray = Array.from({ length: 20 }, () => 0);
+
     return (
         <>
             <Helmet>
@@ -11,14 +14,18 @@ export default function Characters() {
 
             <CharactersBanner />
 
-            <Heading>CHARACTERS</Heading>
-            <Box
-                ml="200px"
-                w="150px"
-                h="150px"
-                bgColor="palevioletred"
-                background="linear-gradient(-45deg, transparent 15px, palevioletred 0)"
-            ></Box>
+            <Box w="100%" minH="100vh" bg="whitesmoke" px="350px">
+                <Grid
+                    templateColumns="repeat(6, 1fr)"
+                    py="50px"
+                    rowGap="50px"
+                    gap="10px"
+                >
+                    {loadingArray.map((dummy, index) => (
+                        <Loading key={index} />
+                    ))}
+                </Grid>
+            </Box>
         </>
     );
 }
